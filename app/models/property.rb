b41a4c00179f951bc15f107f00ec1d1176764f5a
Russell Hallowes-Smith <russellsmith6@gmail.com>
2023-06-29 16:35:44 +0100

    added new listings

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,6 +1,7 @@
 class Property < ApplicationRecord
   belongs_to :user
   has_many_attached :photos
+  validates :photos, presence: true
   has_many :reviews, dependent: :destroy
   has_many :bookings, dependent: :destroy
 end
