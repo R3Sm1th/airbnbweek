2ed8e44cd9ba0d0088e14d779c22f27f852a097c
Russell Hallowes-Smith <russellsmith6@gmail.com>
2023-07-02 10:27:26 +0100

    styling to show

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
+    @featured = Property.order(rating: :desc).first
+    @recent = Property.order(created_at: :desc).last
   end
 end
--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,4 +1,5 @@
 class PropertiesController < ApplicationController
+  skip_before_action :authenticate_user!, only: :index
   before_action :set_property, only: %i[show edit update destroy]
   # before_action :get_bookings, only: %i[next_available]
 
